63f58e90e70fd2572faa9b278db2818e5f821118
renhaofan <yqykrhf@163.com>
2023-04-05 14:54:09 +0800

    chore: add install opengl script

---
+++ install_opengl.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+<<'COMMENT'
+If choose OpenGL when install CUDA, this is not necessary.
+libglu1-mesa-dev ---- Includes headers and static libraries for compiling programs with GLU. 
+mesa-common-dev ---- Mesa-specific OpenGL extensions 
+mesa-utils ---- GL utilities built by Mesa, including glxinfo and glxgears. 
+COMMENT
+sudo apt install -y libglu1-mesa-dev mesa-common-dev mesa-utils
+
+# libraries
+sudo apt install -y freeglut3-dev libglm-dev libassimp-dev libglew-dev
+sudo apt install -y libglfw3 libglfw3-dev libglfw3-doc
